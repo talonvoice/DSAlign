c390a5e542c64bb55d24c65df0071cc72b5359c4
Ryan Hileman <lunixbochs@gmail.com>
2020-01-30 09:13:31 -0800

    change readlink to work on mac

--- bin/align.sh
+++ bin/align.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-approot="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+approot=$(cd "$(dirname "$0")"/.. && pwd)
 source "$approot/venv/bin/activate"
 python "$approot/align/align.py" "$@"
 stty sane
